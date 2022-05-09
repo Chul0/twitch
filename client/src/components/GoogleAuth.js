302777ac9364d5981c5e15084256fe32afa59d0d
Sungchul Park <sungchulpark0@gmail.com>
2022-05-09 12:19:42 -0400

    Create GoogldAuth component and wire Google API lib

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+        //You want to load it just once.
+        window.gapi.load("client:auth2", () => {
+            window.gapi.client.init({
+                cliendId:
+                    "436925754518-ridv01s81s19inioa7f9eu8d0aopopj3.apps.googleusercontent.com",
+                scope: "email", //meaning you only want to access to user's email.
+            });
+        });
+    }
+
     render() {
         return <div>Google Auth</div>;
     }
