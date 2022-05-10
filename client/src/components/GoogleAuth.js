9860bcc6f22ea002a1ba4bc3ac40a1638e00b6f5
Sungchul Park <sungchulpark0@gmail.com>
2022-05-10 12:55:49 -0400

    Updating Auth State with prototype methods

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -10,14 +10,21 @@ class GoogleAuth extends React.Component {
                     clientId:
                         "436925754518-8c7hipcq7hhic3i5umf363m5jq9ilurd.apps.googleusercontent.com",
                     scope: "email",
-                    plugin_name: "streamy",
+                    plugin_name: "streamy", // This parameter is required..
                 })
                 .then(() => {
                     this.auth = window.gapi.auth2.getAuthInstance();
                     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                    // get(), listen() is a prototype method function.
+                    this.auth.isSignedIn.listen(this.onAuthChange);
+                    //listen() is a method you can pass a callback function to, this function will be invoked every time Auth status changes.
                 });
         });
     }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
 
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
