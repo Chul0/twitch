2ebfb67f7d0507da637dea5933f8d321719f08de
Sungchul Park <sungchulpark0@gmail.com>
2022-05-11 11:54:16 -0400

    Passing all pre-defined event handlers from redux-form to input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form"; 
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+    renderInput({ input }) {
+        return <input {...input} />;
+    }
+
     render() {
         return (
             <form>
-                <Field name="title" /> 
-                <Field name="description" /> 
-                
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
             </form>
-        )
+        );
     }
 }
 
 export default reduxForm({
-    form: 'streamCreate' 
-})(StreamCreate); 
+    form: "streamCreate",
+})(StreamCreate);
