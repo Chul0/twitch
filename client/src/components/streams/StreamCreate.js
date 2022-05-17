5775459540fabe2c72cafbe26cfc35064a21afc9
Sungchul Park <sungchulpark0@gmail.com>
2022-05-17 11:52:08 -0400

    Created an actionCreator with POST request

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-    
     renderError({ error, touched }) {
         if (touched && error) {
             return (
@@ -31,9 +30,20 @@ class StreamCreate extends React.Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name="title" component={this.renderInput} label="Enter Title" />
-                <Field name="description" component={this.renderInput} label="Enter Description" />
+            <form
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+                className="ui form error"
+            >
+                <Field
+                    name="title"
+                    component={this.renderInput}
+                    label="Enter Title"
+                />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label="Enter Description"
+                />
                 <button className="ui button primary">Submit</button>
             </form>
         );
@@ -43,8 +53,12 @@ class StreamCreate extends React.Component {
 const validate = (formValues) => {
     const errors = {};
 
-    if (!formValues.title) { errors.title = "You must enter a title" }
-    if (!formValues.description) { errors.description = "You must enter a description"}
+    if (!formValues.title) {
+        errors.title = "You must enter a title";
+    }
+    if (!formValues.description) {
+        errors.description = "You must enter a description";
+    }
 
     return errors;
 };
