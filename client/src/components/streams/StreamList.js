70d1e542ffdb4c56a8be837de76da8a202abe2fa
Sungchul Park <sungchulpark0@gmail.com>
2022-06-08 13:38:22 -0400

    conditianlly show Create Stream Button and link it to the right cmp

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
-import streams from "../../apis/streams";
-
+import { Link } from "react-router-dom";
 class StreamList extends React.Component {
     componentDidMount() {
         this.props.fetchStreams();
         //Fetch streams just one time when this cmp is rendered.
     }
-    
+
     renderAdmin(stream) {
         //helper function for edit&delete buttons
-        if(stream.userId === this.props.currentUserId) {
+        if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
-                        Edit
-                    </button>
-                    <button className="ui button negative">
-                        Delete
-                    </button>
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
                 </div>
-            )
+            );
         }
     }
 
@@ -40,21 +35,35 @@ class StreamList extends React.Component {
         });
     }
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: "right" }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
-    return { 
+    return {
         streams: Object.values(state.streams),
         //Object.values(): built-in js method. take object as an argument, all the values the object has will be pulled out, and be inserted into an array
-        currentUserId: state.auth.userId  //passing userId
+        currentUserId: state.auth.userId, //passing userId
+        isSignedIn: state.auth.isSignedIn, //create another state, isSignedIn
     };
 };
 
