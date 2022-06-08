c4ab14e9ead09b9f99a06c51e191b72acead6b18
Sungchul Park <sungchulpark0@gmail.com>
2022-06-08 13:20:28 -0400

    Associating streams with users: adding userId when creating streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,9 +24,11 @@ export const signOut = () => {
     };
 };
 
-export const createStream = formValues => async dispatch => {
-    
-    const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    //Add redux builtin argument getState: you can access to state and pull out some data.
+    const { userId } = getState().auth; //destructure out userId 
+    const response = await streams.post("/streams", {...formValues, userId});
+    //^take all the form values and combine userId too
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
     //To use async with redux-thunk.. you need to call `dispatch` manually and pass action(either object or function) in it.
