3808073382616cdccdf5db4100d97810c972c0a7
Sungchul Park <sungchulpark0@gmail.com>
2022-06-13 18:05:36 -0400

    Hiding a modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,21 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-               <div className="header">Delete Stream</div>
-               <div className="content">
-                   Are you sure you want to delete this stream?
-               </div>
-               <div className="actions">
-                   <button className="ui primary button">Delete</button>
-                   <button className="ui button">Cancel</button>
-               </div>
+        <div
+            onClick={() => history.push("/")} //programatically redirect user when they want to close the modal
+            className="ui dimmer modals visible active"
+        >
+            <div onClick={(e) => e.stopPropagation()} //stop any parent component's event from firing.
+                    className="ui standard modal visible active">
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>, //seconde argument createPortal
-        document.querySelector('#modal') //every time we render Modal component, rather than showing it as a direct child of the parent cmp, it will be rendered into the div with an id:modal
+        document.querySelector("#modal") //every time we render Modal component, rather than showing it as a direct child of the parent cmp, it will be rendered into the div with an id:modal
     );
 };
 
