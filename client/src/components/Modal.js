c60fc9fef5f5e750c5d5e3633f96d82d456b9a31
Sungchul Park <sungchulpark0@gmail.com>
2022-06-13 18:00:08 -0400

    Modal styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,7 +5,14 @@ const Modal = (props) => {
     return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                asjdfldjsflk
+               <div className="header">Delete Stream</div>
+               <div className="content">
+                   Are you sure you want to delete this stream?
+               </div>
+               <div className="actions">
+                   <button className="ui primary button">Delete</button>
+                   <button className="ui button">Cancel</button>
+               </div>
             </div>
         </div>, //seconde argument createPortal
         document.querySelector('#modal') //every time we render Modal component, rather than showing it as a direct child of the parent cmp, it will be rendered into the div with an id:modal
