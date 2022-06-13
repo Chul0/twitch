a9499b75f198f456161f2f465c367836fe8e409d
Sungchul Park <sungchulpark0@gmail.com>
2022-06-13 18:17:58 -0400

    Made modal reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,16 +8,13 @@ const Modal = (props) => {
             onClick={() => history.push("/")} //programatically redirect user when they want to close the modal
             className="ui dimmer modals visible active"
         >
-            <div onClick={(e) => e.stopPropagation()} //stop any parent component's event from firing.
-                    className="ui standard modal visible active">
-                <div className="header">Delete Stream</div>
-                <div className="content">
-                    Are you sure you want to delete this stream?
-                </div>
-                <div className="actions">
-                    <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
-                </div>
+            <div
+                onClick={(e) => e.stopPropagation()} //stop any parent component's event from firing.
+                className="ui standard modal visible active"
+            >
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>, //seconde argument createPortal
         document.querySelector("#modal") //every time we render Modal component, rather than showing it as a direct child of the parent cmp, it will be rendered into the div with an id:modal
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,22 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    ); //^ This is a helper Fragment to pass to Modal as props
+    //You can use <></> instead of <React.Fragment></React.Fragment>
+
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     );
 };
