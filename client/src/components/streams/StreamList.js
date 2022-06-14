116dcda44306a8f7c4cc223ca54259bd9efacf26
Sungchul Park <sungchulpark0@gmail.com>
2022-06-13 22:52:33 -0400

    StreamShow

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -28,10 +28,11 @@ class StreamList extends React.Component {
                 <div className="item" key={stream.id}>
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
-                    <div className="content">
-                        {stream.title}
+                    <Link to={`/streams/${stream.id}`} className="header">
+                        {stream.title} 
                         <div className="description">{stream.description}</div>
-                    </div>
+                    </Link>
+                    {/* Direct user to designated stream/:id using <Link> */}
                 </div>
             );
         });
